5835425261cf14b81ecddf71cbb47e2a0726e2db
Andrea Falconi <andrea.falconi@gmail.com>
2017-07-21 15:45:16 +0200

    refactor pos-n to avoid deps that cant be moved to util.

--- components/server/src/main/java/ome/smuggler/core/types/PositiveN.java
+++ components/server/src/main/java/ome/smuggler/core/types/PositiveN.java
@@ -1,26 +1,51 @@
 package ome.smuggler.core.types;
 
-import static util.error.Exceptions.getOrThrow;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import util.object.Wrapper;
 
 /**
  * A positive natural number, i.e. an integer greater than zero.
- * An instance of this object can only be obtained through a parser so that it
- * is impossible to construct an invalid value.
  */
 public class PositiveN extends Wrapper<Long> {
 
-    public static PositiveN of(long positiveValue) {
-        return getOrThrow(
-                    ValueParserFactory
-                    .positiveInt(String.valueOf(positiveValue))
-                    .mapLeft(IllegalArgumentException::new)
-               );
+    /**
+     * Test to check if a {@code long} can be used to instantiate a
+     * {@link PositiveN}.
+     */
+    public static final Predicate<Long> isValid = x -> x > 0;
+
+    /**
+     * Instantiate a {@link PositiveN} from the given value only if it's
+     * positive.
+     * @param value the value to use.
+     * @return a {@link PositiveN} wrapping the given value or empty if the
+     * value isn't positive.
+     */
+    public static Optional<PositiveN> from(long value) {
+        return isValid.test(value) ? Optional.of(new PositiveN(value))
+                                   : Optional.empty();
     }
-    
+
+    /**
+     * Instantiate a {@link PositiveN} from a positive value.
+     * @param positiveValue the value to use.
+     * @return a {@link PositiveN} wrapping the given value.
+     * @throws IllegalArgumentException if the value isn't positive.
+     */
+    public static PositiveN of(long positiveValue) {
+        return from(positiveValue)
+              .orElseThrow(() -> new IllegalArgumentException(
+                            "non-positive value: " + positiveValue));
+    }
+
     protected PositiveN(Long wrappedValue) {
         super(wrappedValue);
     }
 
 }
+/* Design Debt.
+ * Nat and PositiveN duplicate code. They can be refactored using a generic
+ * value-builder with `from` and `of` methods.
+ */
