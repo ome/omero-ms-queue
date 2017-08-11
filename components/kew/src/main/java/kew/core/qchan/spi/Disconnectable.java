cefb828099515f17f1436645ea8323fe506242f7
Andrea Falconi <andrea.falconi@gmail.com>
2017-08-11 12:38:20 +0200

    move auto-closable stuff to qchan.

--- components/kew/src/main/java/kew/core/qchan/spi/Disconnectable.java
+++ components/kew/src/main/java/kew/core/qchan/spi/Disconnectable.java
@@ -1,15 +1,15 @@
-package kew.core.msg;
+package kew.core.qchan.spi;
 
 import static util.error.Exceptions.unchecked;
 
 /**
  * Convenience interface to provide auto-closable functionality for
- * channel sources and sinks.
+ * queue consumers, producers, and sessions.
  */
 public interface Disconnectable extends AutoCloseable {
 
     /**
-     * Disconnects from the underlying channel, releasing any associated
+     * Disconnects from the underlying entity, releasing any associated
      * resources. This method is the same as {@link #close() close} but
      * masks any exception as a runtime (unchecked) exception and throws
      * it as such without any wrapping.
--- components/kew/src/test/java/kew/core/qchan/spi/DisconnectableTest.java
+++ components/kew/src/test/java/kew/core/qchan/spi/DisconnectableTest.java
@@ -1,4 +1,4 @@
-package kew.core.msg;
+package kew.core.qchan.spi;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
