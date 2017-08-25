48785e0672e485684d61814900f0903696fc0875
Andrea Falconi <andrea.falconi@gmail.com>
2017-08-25 17:56:53 +0200

    implement embedded broker transport config.

---
+++ components/kew-artemis/src/main/java/kew/providers/artemis/config/EmbeddedBrokerTransportConfig.java
@@ -0,0 +1,84 @@
+package kew.providers.artemis.config;
+
+import static java.util.Objects.requireNonNull;
+import static kew.providers.artemis.config.EmbeddedTransportProps.serverId;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
+
+import util.object.Builder;
+import util.types.PositiveN;
+
+/**
+ * Holds the configuration to make Artemis work with embedded connections.
+ * Specifically an instance of this class configures Artemis with an "in-vm"
+ * acceptor and a matching connector so that you can connect to Artemis from
+ * within the same JVM process in which Artemis is embedded.
+ */
+public class EmbeddedBrokerTransportConfig {
+
+    private static final AtomicInteger currentServerId = new AtomicInteger();
+
+    private static int nextServerId() {
+        return currentServerId.incrementAndGet();
+    }
+
+    private final int embeddedServerId;
+    private final EmbeddedTransportConfig embeddedAcceptor;
+    private final EmbeddedTransportConfig embeddedConnector;
+
+    /**
+     * Creates a new instance.
+     */
+    public EmbeddedBrokerTransportConfig() {
+        embeddedServerId = nextServerId();
+        embeddedAcceptor = makeTransport(embeddedServerId);
+        embeddedConnector = makeTransport(embeddedServerId);
+    }
+
+    private EmbeddedTransportConfig makeTransport(int embeddedServerId) {
+        return Builder.make(EmbeddedTransportConfig::new)
+                      .with(serverId(PositiveN.of(embeddedServerId)))
+                      .apply(null);
+    }
+
+    /**
+     * @return the ID shared by both the embedded acceptor and connector.
+     */
+    public int embeddedServerId() {
+        return embeddedServerId;
+    }
+
+    /**
+     * @return the embedded acceptor.
+     */
+    public TransportConfiguration embeddedAcceptor() {
+        return embeddedAcceptor.get();
+    }
+
+    /**
+     * @return the embedded connector.
+     */
+    public TransportConfiguration embeddedConnector() {
+        return embeddedConnector.get();
+    }
+
+    /**
+     * Adds the embedded acceptor/connector pair to the specified configuration.
+     * @param cfg the configuration object that will be used to start the
+     *            embedded Artemis server.
+     * @return the input configuration with the new settings.
+     * @throws NullPointerException if the argument is {@code null}.
+     */
+    public Configuration embeddedTransport(Configuration cfg) {
+        requireNonNull(cfg, "cfg");
+
+        cfg.addAcceptorConfiguration(embeddedAcceptor());
+        cfg.addConnectorConfiguration(embeddedConnector().getName(),
+                                      embeddedConnector());
+        return cfg;
+    }
+
+}
---
+++ components/kew-artemis/src/test/java/kew/providers/artemis/config/EmbeddedBrokerTransportConfigTest.java
@@ -0,0 +1,62 @@
+package kew.providers.artemis.config;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
+import org.apache.activemq.artemis.core.remoting.impl.invm.TransportConstants;
+import org.junit.Test;
+
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
+
+public class EmbeddedBrokerTransportConfigTest {
+
+    private static int getServerId(TransportConfiguration tc) {
+        return (int) tc.getParams()
+                       .get(TransportConstants.SERVER_ID_PROP_NAME);
+    }
+
+
+    @Test
+    public void generateSequentialServerIds() {
+        int[] expected = new int[] { 1, 2, 3 };
+
+        IntUnaryOperator f = x -> new EmbeddedBrokerTransportConfig()
+                                        .embeddedServerId();
+        int[] actual = IntStream.of(expected).map(f).toArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void connectorAndAcceptorHaveSameServerId() {
+        EmbeddedBrokerTransportConfig cfg = new EmbeddedBrokerTransportConfig();
+        int serverId = cfg.embeddedServerId();
+        int acceptorId = getServerId(cfg.embeddedAcceptor());
+        int connectorId = getServerId(cfg.embeddedConnector());
+
+        assertThat(serverId, is(acceptorId));
+        assertThat(serverId, is(connectorId));
+    }
+
+    @Test
+    public void configureConnectorAcceptorPair() {
+        EmbeddedBrokerTransportConfig tc = new EmbeddedBrokerTransportConfig();
+        Configuration actual = CoreConfigFactory.empty()
+                                                .with(tc::embeddedTransport)
+                                                .apply(null);
+
+        assertThat(actual.getAcceptorConfigurations().size(), is(1));
+        assertTrue(
+                actual.getAcceptorConfigurations().contains(
+                        tc.embeddedAcceptor()
+                ));
+        assertThat(actual.getConnectorConfigurations().size(), is(1));
+        assertThat(actual.getConnectorConfigurations().values().toArray()[0],
+                   is(tc.embeddedConnector()));
+    }
+
+}
