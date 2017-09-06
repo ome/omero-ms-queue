e481e6c69ed90cbcd989ee3c6fc3d9a28f9f0f48
Andrea Falconi <andrea.falconi@gmail.com>
2017-09-06 10:11:58 +0200

    improve test coverage.

--- components/kew-artemis/src/test/java/kew/providers/artemis/config/transport/AcceptorConfigTest.java
+++ components/kew-artemis/src/test/java/kew/providers/artemis/config/transport/AcceptorConfigTest.java
@@ -1,9 +1,15 @@
 package kew.providers.artemis.config.transport;
 
+import static org.mockito.Mockito.*;
+
 import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.core.config.Configuration;
+import org.junit.Test;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class AcceptorConfigTest
         extends BaseTransportConfigTest
@@ -19,6 +25,29 @@ public class AcceptorConfigTest
     protected void addTransportToOverride(Configuration cfg,
                                           TransportConfiguration tc) {
         cfg.addAcceptorConfiguration(tc);
+    }
+
+    @Test
+    public void tolerateNullAcceptorConfigurations() {
+        Configuration config = mock(Configuration.class);
+        when(config.getAcceptorConfigurations()).thenReturn(null);
+        transportConfig(config);
+
+
+        verify(config).getAcceptorConfigurations();
+        verify(config).addAcceptorConfiguration(any());
+    }
+
+    @Test
+    public void tolerateNullTransportInAcceptorConfigurations() {
+        Configuration config = mock(Configuration.class);
+        Set<TransportConfiguration> acceptors = new HashSet<>();
+        acceptors.add(null);
+        when(config.getAcceptorConfigurations()).thenReturn(acceptors);
+        transportConfig(config);
+
+        verify(config, times(2)).getAcceptorConfigurations();
+        verify(config).addAcceptorConfiguration(any());
     }
 
 }
